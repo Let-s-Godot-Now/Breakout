f91665e4f9186591515412ed216b3fb3c73bffe9
sout233 <2273466101@qq.com>
2023-07-28 23:15:18 +0800

    Fix: brick

--- scripts/panddle.gd
+++ scripts/panddle.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var plane = get_node("Plane")
 
-var speed = 1000
+var speed = 500
 var screen_size = Vector2.ZERO
 var plane_size = Vector2.ZERO
 
@@ -15,7 +15,13 @@ func _ready():
 
 func _process(delta):
 	if Input.is_action_pressed("move_left"):
-		position.x -= speed * delta
+		if Input.is_action_pressed("faster_shift"):
+			position.x -= speed * delta * 2
+		else:
+			position.x -= speed * delta
 	elif Input.is_action_pressed("move_right"):
-		position.x+=speed*delta
-	position.x=clamp(position.x,plane_size.x/2,screen_size.x-plane_size.x/2)
+		if Input.is_action_pressed("faster_shift"):
+			position.x += speed * delta * 2
+		else:
+			position.x += speed * delta
+	position.x = clamp(position.x, plane_size.x / 2, screen_size.x - plane_size.x / 2)
--- tscn/brick.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-var flag=false
-
-func _on_up_area_entered(area:Area2D):
-	if area.name == "BallArea":
-		queue_free()
