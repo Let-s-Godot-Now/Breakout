7c637c3d938b9a87b525b6c949c91df27e59b8f8
sout233 <2273466101@qq.com>
2023-07-30 15:35:28 +0800

    完善砖块生成器

--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -3,12 +3,23 @@ extends Node
 @onready var ball = $"../Ball"
 @onready var paddle = $"../Paddle"
 @onready var bricks = $"../Bricks"
+# The memory var when game start
+@onready var mem_start_horiz_gen_num = $"../Bricks".brick_horiz_gen_num
+@onready var mem_start_vert_gen_num = $"../Bricks".brick_vert_gen_num
+
+@export var max_horiz_num := 8
+@export var max_vert_num := 6
+
 
 func update_level():
 	print("win")
 	GlobalValue.score = 0
 	ball.reset()
 	paddle.reset()
-	bricks.brick_vert_gen_num += 1
-	bricks.brick_horiz_gen_num += 1
+	if bricks.brick_vert_gen_num > max_vert_num or bricks.brick_horiz_gen_num > max_horiz_num:
+		bricks.brick_vert_gen_num = mem_start_vert_gen_num
+		bricks.brick_horiz_gen_num = mem_start_horiz_gen_num
+	else:
+		bricks.brick_vert_gen_num += 1
+		bricks.brick_horiz_gen_num += 1
 	bricks.gen_bricks()
