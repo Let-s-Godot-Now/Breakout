146382d1045da173f5f3179a346caaa7fa9011c9
sout233 <2273466101@qq.com>
2023-08-03 00:28:17 +0800

    完善擦球机制

--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -2,13 +2,15 @@ extends Node2D
 
 @onready var plane = $Plane
 
-var speed = 500
+@export var speed = 500
+var rem_speed_velocity: float
 var screen_size = Vector2.ZERO
 var plane_size = Vector2.ZERO
 
 
 func _ready():
 	set_clamp()
+
 
 func set_clamp():
 	screen_size = get_viewport_rect().size
@@ -25,11 +27,17 @@ func _process(delta):
 	if Input.is_action_pressed("move_left"):
 		if Input.is_action_pressed("faster_shift"):
 			position.x -= speed * delta * 2
+			rem_speed_velocity = -speed * delta * 2
 		else:
 			position.x -= speed * delta
+			rem_speed_velocity = -speed * delta
 	elif Input.is_action_pressed("move_right"):
 		if Input.is_action_pressed("faster_shift"):
 			position.x += speed * delta * 2
+			rem_speed_velocity = speed * delta * 2
 		else:
 			position.x += speed * delta
+			rem_speed_velocity = speed * delta
+	else:
+		rem_speed_velocity = 0
 	position.x = clamp(position.x, plane_size.x / 2, screen_size.x - plane_size.x / 2)
