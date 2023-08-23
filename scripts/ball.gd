4a0ec84436e69683aefc4d5f805f2bd55672b763
HIM049 <him_049@163.com>
2023-08-23 16:37:50 +0800

    添加注释，微调代码，修改摩擦反弹量

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,89 +1,119 @@
 extends Node2D
 
+# 在节点准备就绪时预加载 CollisionShape2D 并将其存储在变量中
 @onready var colli_shape: CollisionShape2D = $BallArea/CollisionShape2D
+# 在节点准备就绪时预加载 AnimationPlayer 并将其存储在变量中
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+# 在节点准备就绪时预加载 brick_arr、paddle 和 walls 并将其存储在变量中
 @onready var brick_arr: Node2D = GlobalValue.bricks
 @onready var paddle: Node2D = GlobalValue.paddle
 @onready var walls: Node2D = GlobalValue.walls
 
+# 预加载碰撞音效
 var knock_sound1 = preload("res://res/audio/ball_knock_1.ogg")
 var knock_sound2 = preload("res://res/audio/ball_knock_2.ogg")
 
+# 导出变量，控制球的速度
 @export var speed := 600
+# 球的速度向量
 var velocity := Vector2.ZERO
 
-
-func _process(delta):
+# 在每帧处理中调用
+func _physics_process(delta):
+	# 如果游戏已经开始
 	if GlobalValue.start_game == true:
+		# 根据速度和时间增量移动球的位置
 		position += velocity * delta
+		# 使球旋转（如果需要）
 		# rotation_degrees += 10
 
-
+# 重置球的位置和速度
 func reset() -> void:
+	# 将球的位置重置为视口的中心
 	position = get_viewport_rect().size / 2
-	velocity = Vector2.DOWN * speed
+	# 设置球的初始速度向下
+	velocity = Vector2.UP * speed
 
-
+# 将球移动回视口中心
 func back2center() -> void:
+	# 如果 leyline_lock 未锁定
 	if GlobalValue.leyline_lock == false:
+		# 锁定 leyline_lock，防止同时触发多次
 		GlobalValue.leyline_lock = true
+		# 停止球的速度
 		velocity = Vector2.ZERO
+		# 触发摄像机震动效果
 		GlobalValue.camera.shake(0.8, 30, 15)
+		# 重置球的旋转角度
 		rotation = 0
 
+		# 等待一段时间
 		await get_tree().create_timer(1.0).timeout
+		# 将球的纵坐标设置为视口的纵坐标
 		position.y = get_viewport_rect().size.y
+		# 调用 bounce 函数使球反弹
 		bounce(
 			position.angle_to_point(
 				Vector2(get_viewport_rect().size.x / 2, get_viewport_rect().size.y / 2)
 			),
 			9
 		)
+		# 再次触发摄像机震动效果
 		GlobalValue.camera.shake(0.8, 30, 15)
+		# 解锁 leyline_lock
 		GlobalValue.leyline_lock = false
 
-
+# 当球进入 BallArea 区域时调用
 func _on_ball_area_entered(area: Area2D) -> void:
+	# 根据不同的区域名执行不同的操作
 	match area.name:
 		"PaddleNoodle":
+			# 如果球碰到板子并且板子没有速度
 			if paddle.rem_speed_velocity == 0:
+				# 以 PI/2 的角度反弹
 				bounce(-PI / 2, 5)
 			else:
-				bounce(-PI / 2 + paddle.rem_speed_velocity / 4, 9)
+				# 根据板子的速度反弹
+				bounce(-PI / 2 + paddle.rem_speed_velocity / 8, 9)
 		"Up":
+			# 以 PI/2 的角度反弹
 			bounce(-PI / 2, 3)
 		"Down":
+			# 以 -PI/2 的角度反弹
 			bounce(PI / 2, 3)
 		"Left":
+			# 以 PI 的角度反弹
 			bounce(PI, 3)
 		"Right":
+			# 不改变角度反弹
 			bounce(0, 3)
 		# "leyline of the void":
+		# 	# 在这里执行一些操作来重置游戏状态
+		# 	# ...
 		# 	reset()
-		# 	$"../Paddle".reset()
-		# 	# if not GlobalValue.leyline_lock:
-		# 	# 	GlobalValue.leyline_lock = true
-		# 	# 	GlobalValue.camera.shake(0.8, 30, 15)
-
-		# 	# 	await get_tree().create_timer(3.0).timeout
-		# 	# 	position.y = get_viewport_rect().size.y
-		# 	# 	velocity = Vector2.ZERO
-		# 	# 	rotation = 0
-		# 	# 	bounce(-PI / 2, 9)
-		# 	# 	GlobalValue.camera.shake(0.8, 30, 15)
-		# 	# 	GlobalValue.leyline_lock = false
+		# 	paddle.reset()
+		# 	# ...
+	
+	# 如果球碰到了砖块
 	if area.get_parent().get_parent() == brick_arr:
+		# 创建并实例化爆炸粒子效果
 		var particles = load("res://tscn/boom_particles.tscn").instantiate()
 		area.get_parent().get_parent().add_child(particles)
 		particles.position = area.get_parent().position
 		particles.get_node("Particles").emitting = true
 
+		# 触发摄像机震动效果
 		GlobalValue.camera.shake(0.3, 30, 15)
+	# 如果球碰到了边界墙
 	elif area.get_parent().get_parent() == walls:
+		# 触发摄像机震动效果
 		GlobalValue.camera.shake(0.3, 30, 10)
+	# 如果球碰到了板子
 	elif area.get_parent() == paddle:
+		# 触发摄像机震动效果
 		GlobalValue.camera.shake(0.2, 20, 5)
-
+	
+	# 随机选择一个碰撞音效并播放
 	var sound
 	if randf() > 0.5:
 		sound = knock_sound1
@@ -93,10 +123,13 @@ func _on_ball_area_entered(area: Area2D) -> void:
 	$AudioStreamPlayer2D.stream = sound
 	$AudioStreamPlayer2D.play()
 
-
+# 球的反弹函数
 func bounce(dir, degree) -> void:
+	# 随机调整方向
 	randomize()
 	dir += randf_range(-PI / degree, PI / degree)
+	# 根据方向和速度计算新的速度向量
 	rotation = dir
 	velocity = Vector2(1, 0).rotated(dir) * speed
+	# 播放反弹动画
 	animation_player.play("ball_bounce")
--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -1,61 +1,97 @@
 extends Node2D
 
+# 继承自 Node2D 节点
+
+# 在节点就绪时预加载 Plane 节点，并将其存储在变量中
 @onready var plane := $Plane
+# 在节点就绪时预加载按钮 A，并将其存储在变量中
 @onready var button_a: Button = $"../UICanvasLayer/Controllers/ButtonA"
+# 在节点就绪时预加载按钮 D，并将其存储在变量中
 @onready var button_d = $"../UICanvasLayer/Controllers/ButtonD"
+# 在节点就绪时预加载按钮 Shift，并将其存储在变量中
 @onready var button_shift := $"../UICanvasLayer/Controllers/ButtonShift"
+# 预加载 ghost_paddle.tscn 场景资源，并将其存储在变量中
 @onready var ghost_paddle := preload("res://tscn/ghost_paddle.tscn")
 
+# 导出变量，控制平移速度
 @export var speed := 500
+# 剩余速度（用于反向操作）
 var rem_speed_velocity: float = 0
+# 屏幕尺寸
 var screen_size := Vector2.ZERO
+# 平板尺寸
 var plane_size := Vector2.ZERO
 
-
+# 当节点准备就绪时调用
 func _ready():
+	# 调用函数来设置平板移动的边界限制
 	set_clamp()
 
+# 在每帧处理中调用
+func _physics_process(delta):
+	# 将剩余速度设置为 0
+	rem_speed_velocity = 0
 
-func _process(delta):
-	if Input.is_action_pressed("move_left") or button_a.button_pressed:
-		if Input.is_action_pressed("faster_shift") or button_shift.button_pressed:
+	# 如果向左移动的动作被按下
+	if Input.is_action_pressed("move_left"):
+		# 如果同时按下“加速”动作或按钮 Shift
+		if Input.is_action_pressed("faster_shift"):
+			# 向左移动，并根据 delta 和速度增加剩余速度
 			position.x -= speed * delta * 2
 			rem_speed_velocity = -speed * delta * 2
 		else:
+			# 向左移动，并根据 delta 和速度增加剩余速度
 			position.x -= speed * delta
 			rem_speed_velocity = -speed * delta
-	elif Input.is_action_pressed("move_right") or button_d.button_pressed:
-		if Input.is_action_pressed("faster_shift") or button_shift.button_pressed:
+
+	# 如果向右移动的动作被按下
+	if Input.is_action_pressed("move_right"):
+		# 如果同时按下“加速”动作或按钮 Shift
+		if Input.is_action_pressed("faster_shift"):
+			# 向右移动，并根据 delta 和速度增加剩余速度
 			position.x += speed * delta * 2
 			rem_speed_velocity = speed * delta * 2
 		else:
+			# 向右移动，并根据 delta 和速度增加剩余速度
 			position.x += speed * delta
 			rem_speed_velocity = speed * delta
-	else:
-		rem_speed_velocity = 0
+	# 如果没有按下移动动作或按钮
+	
+	# 限制平板的水平位置在屏幕范围内
 	position.x = clamp(position.x, plane_size.x / 2, screen_size.x - plane_size.x / 2)
 
-
+# 设置平板的移动边界限制
 func set_clamp() -> void:
+	# 获取当前视口的尺寸作为屏幕尺寸
 	screen_size = get_viewport_rect().size
+	# 获取 Plane 节点的尺寸作为平板尺寸
 	plane_size = plane.get_rect().size
+	# 根据缩放设置平板的 x 尺寸
 	plane_size.x *= scale.x
 
-
+# 重置平板的位置
 func reset() -> void:
+	# 将平板位置重置为特定值
 	position.x = 576
 
-
+# 生成幽灵平板
 func ghost_gen() -> void:
+	# 如果向左或向右移动的动作被按下，或者按钮 A 或按钮 D 被按下
 	if (
 		Input.is_action_pressed("move_left")
 		or Input.is_action_pressed("move_right")
 		or button_a.button_pressed
 		or button_d.button_pressed
 	):
+		# 如果同时按下“加速”动作或按钮 Shift
 		if Input.is_action_pressed("faster_shift") or button_shift.button_pressed:
+			# 实例化一个幽灵平板场景
 			var ghost_paddle_tscn = ghost_paddle.instantiate()
+			# 设置幽灵平板的位置与当前平板位置相同
 			ghost_paddle_tscn.position = position
+			# 设置幽灵平板的 x 缩放与当前平板的 x 缩放相同
 			ghost_paddle_tscn.scale.x = scale.x
+			# 设置幽灵平板的透明度以创建幽灵效果
 			ghost_paddle_tscn.get_node("Sprite2D").modulate.a = 0.7
+			# 将幽灵平板添加到当前节点的父节点中
 			get_parent().add_child(ghost_paddle_tscn)
